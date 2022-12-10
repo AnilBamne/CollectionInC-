e1ad71635171ee7e4f1d56194a8d69f48d8a1c79
AnilBamne <anilbamne20@gmail.com>
2022-12-10 21:11:35 +0530

    Adding:List code

---
+++ CollectionInCSharp/CollectionDemos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionInCSharp
+{
+    public class CollectionDemos
+    {
+        ///<summary>
+        ///LIST
+        ///creating a static method ListDemo,performing list operations 
+        /// </summary>
+        public static void ListDemo()
+        {
+            ///<summary>
+            ///creating a list of strings
+            ///Add method is used to add elements to list
+            /// </summary>
+            List<string> list = new List<string>();
+            list.Add("Anil");
+            list.Add("Bamne");
+            list.Add("From");
+            list.Add("Bidar");
+            list.Add("Karnataka");
+            list.Add("India");
+
+            Console.WriteLine("\nElfements in list are :\n");
+            //Iterating over list to print elements of list
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //list.Remove to remove elements from list 
+            list.Remove("India");                           //removing by elements
+            list.RemoveAt(3);                               //removing by element index
+            Console.WriteLine("\nAfter removing Elfements in list are :\n");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- CollectionInCSharp/Program.cs
+++ CollectionInCSharp/Program.cs
@@ -6,7 +6,21 @@ namespace CollectionInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Collection");
+            Console.WriteLine("Welcome to Collection\n");
+            runAgain:
+            Console.WriteLine("Select option to execute\n1:List\n2:Array\n");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CollectionDemos.ListDemo();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            goto runAgain;
         }
     }
 }
